38c75618f40e26927f1cdaf09c8fa0162f1dfd7c
xhuanhuan <805870540@qq.com>
2016-11-13 17:10:58 +0800

    add es6test.js

---
+++ es6test.js
@@ -0,0 +1,12 @@
+var timer = {
+  seconds: 0,
+  start () {
+    setInterval(() => {
+      this.seconds++
+    }, 1000)
+  }
+}
+timer.start()
+setTimeout(function () {
+  console.log(timer.seconds)
+}, 3500)
