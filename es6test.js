9c16a5f2f872e7053df5726571da026eec6f0668
xhuanhuan <805870540@qq.com>
2016-11-14 16:34:58 +0800

    3.3.1ended and 3.3.2started：destructuring arrays

--- es6test.js
+++ es6test.js
@@ -32,3 +32,16 @@ var { pseudonym } = character//var  pseudonym  = character.pseudonym
 var { pseudonym, name } = character// pseudonym  = character.pseudonym,name = character.name
 var { pseudonym: alias } = character//alias=character.pseudonym
 var { metadata: { gender } } = character//gender=character.metadata.gender
+var { metadata: { gender : metagender } } = character//metagender=character.metadata.gender
+console.log(character.boots)// <- undefined
+console.log(character['boots'])// <- undefined
+var { boots } = character
+console.log(boots)// <- undefined
+//A destructured declaration accessing a nested property of a parent object that’s null or undefined will throw an Exception
+var { batmobile: { gear } } = character// <- Exception
+var { missing } = null// <- Exception
+//you can provide default values for those cases where the value is undefined
+var { boots = true } = character
+console.log(boots)// <- true
+var { metadata: { enemy = 'Satan' } } = character
+console.log(enemy)// <- 'Satan'
