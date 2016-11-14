8f449a7f3e65a29fb454a5d76b1665cdda58151d
xhuanhuan <805870540@qq.com>
2016-11-14 16:23:56 +0800

    3.3.1 Destructuring Objects

--- es6test.js
+++ es6test.js
@@ -10,3 +10,25 @@ timer.start()
 setTimeout(function () {
   console.log(timer.seconds)
 }, 3500)
+//==========================
+[1, 2, 3].map(value => { number: value, verified: true })// <- SyntaxError
+[1, 2, 3].map(value => ({ number: value, verified: true }))
+/*[
+  { number: 1, verified: true },
+  { number: 2, verified: true },
+  { number: 3, verified: true }]
+*/
+//==================
+var character = {
+  name: 'Bruno',
+  pseudonym: 'Batman',
+  metadata: {
+    age: 34,
+    gender: 'male'
+  },
+  batarang: ['gas pellet', 'bat-mobile control', 'bat-cuffs']
+}
+var { pseudonym } = character//var  pseudonym  = character.pseudonym
+var { pseudonym, name } = character// pseudonym  = character.pseudonym,name = character.name
+var { pseudonym: alias } = character//alias=character.pseudonym
+var { metadata: { gender } } = character//gender=character.metadata.gender
